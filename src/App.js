0936b0ea37298d5c8b20f00c0f09fa9978ef022e
Richárd Nagy <nagy.richard@thisisdone.com>
2022-12-03 22:22:20 +0100

    Correctly submit answer, dont reset base 4 colros

--- src/App.js
+++ src/App.js
@@ -1,68 +1,81 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
-const colors = ["red", "blue", "yellow", "white", "green", "brown", "purple", "black"];
+const colors = ["...", "red", "blue", "yellow", "white", "green", "brown", "purple", "black"];
 
 const getRandomColors = () => {
     const number = () => {
-        return Math.floor(Math.random() * 8);
+        return Math.floor(Math.random() * 8 + 1);
     };
 
     return [colors[number()], colors[number()], colors[number()], colors[number()]];
 };
 
 function App() {
-    const [choosenColors, setChoosenColors] = useState([null, null, null, null]);
     const [numberOfGuesses, setNumberOfGuesses] = useState(0);
+    const [oldGuesses, setOldGuesses] = useState([]);
+    const randomColors = useRef(getRandomColors());
+
+    const [selected, setSelected] = useState(Array.from(Array(4)).fill("..."));
 
     const chooseColor = (index, color) => {
-        const array = choosenColors;
+        const array = selected;
         array[index] = color;
-        setChoosenColors(array);
+        setSelected(() => [...array]);
     };
 
-    const testColors = () => {
-        if (choosenColors.includes(null)) {
+    const submit = (event) => {
+        event.preventDefault();
+
+        if (selected.includes("...")) {
             console.log("You must choose all four colors first!");
             return;
         }
+        setSelected(() => [...Array.from(Array(4)).fill("...")]);
         setNumberOfGuesses((oldState) => oldState + 1);
-        console.log(choosenColors);
+        setOldGuesses((oldState) => [...oldState, selected]);
     };
 
     return (
         <>
             <h2>Number of Guesses: {numberOfGuesses}</h2>
             <ul>
-                {getRandomColors().map((e, i) => {
+                {randomColors.current.map((e, i) => {
                     return <li key={i}>{e}</li>;
                 })}
             </ul>
-            {Array.from(Array(4)).map((_, i) => {
-                return (
-                    <select
-                        key={i}
-                        name={`colors${i}`}
-                        id={`colors${i}`}
-                        value={undefined}
-                        onChange={(event) => {
-                            chooseColor(i, event.target.value);
-                        }}
-                    >
-                        <option hidden defaultValue>
-                            Select Color
-                        </option>
-                        {colors.map((color) => {
-                            return (
-                                <option key={color} value={color}>
-                                    {color}
+            <form>
+                {Array.from(Array(4)).map((_, i) => {
+                    return (
+                        <select
+                            value={selected[i]}
+                            onChange={(e) => chooseColor(i, e.target.value)}
+                        >
+                            {colors.map((value) => (
+                                <option value={value} key={value}>
+                                    {value}
                                 </option>
-                            );
-                        })}
-                    </select>
-                );
-            })}
-            <button onClick={() => testColors()}>test my guesses</button>
+                            ))}
+                        </select>
+                    );
+                })}
+
+                <button type="button" onClick={(e) => submit(e)}>
+                    Submit
+                </button>
+            </form>
+            <br />
+            <ul>
+                {oldGuesses.map((guesses) => {
+                    return (
+                        <li>
+                            {guesses.map((guess) => {
+                                return `${guess}, `;
+                            })}
+                        </li>
+                    );
+                })}
+            </ul>
         </>
     );
 }
